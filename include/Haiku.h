2f1b40de172edb9d30e6c12854deef050bfdbe7e
Jon <jonathan.woolfrey@gmail.com>
2021-10-18 16:53:11 +0200

    A simple poem in the style of a haiku

---
+++ include/Haiku.h
@@ -0,0 +1,8 @@
+#include <iostream>
+
+void haiku()
+{
+	std::cout << "Worker bees can leave." << std::endl;
+	std::cout << "Even drones can fly away." << std::endl;
+	std::cout << "The Queen is their slave." << std::endl;
+}
