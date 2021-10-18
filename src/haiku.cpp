f8f0dce3465f745b83f460819b4129da14b6a661
Jon <jonathan.woolfrey@gmail.com>
2021-10-18 16:51:56 +0200

    Now relies on Haiku.h

--- src/haiku.cpp
+++ src/haiku.cpp
@@ -1,11 +1,7 @@
-#include <iostream>
+#include <Haiku.h>
 
 int main(int arc, char  *argv[])
-{
-
-	std::cout << "Worker bees can leave." << std::endl;
-	std::cout << "Even drones can fly away." << std::endl;
-	std::cout << "The Queen is their slave." << std::endl;
-	
+{	
+	haiku();
 	return 0;
 }
