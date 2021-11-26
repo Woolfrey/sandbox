ec2547b8ecb57bb9266f3442157e8974ba2a5e85
Jon <jonathan.woolfrey@gmail.com>
2021-11-26 15:47:40 +0100

    Returned rotation matrix incomplete - now fixed

--- include/CartesianTrajectory.h
+++ include/CartesianTrajectory.h
@@ -1,14 +1,14 @@
 #ifndef CARTESIANTRAJECTORY_H_
 #define CARTESIANTRAJECTORY_H_
-#include <Quintic.h>										// Custom trajectory class
-#include <CubicSpline.h>									// Custom trajectory class
+#include <Quintic.h>									// Custom trajectory class
+#include <CubicSpline.h>								// Custom trajectory class
 
 class CartesianTrajectory
 {
 	public:
-		CartesianTrajectory() {};							// Empty constructor
+		CartesianTrajectory() {};						// Empty constructor
 		
-		CartesianTrajectory(	const yarp::sig::Matrix &start_pose,			// Basic constructor
+		CartesianTrajectory(	const yarp::sig::Matrix &start_pose,		// Basic constructor
 					const yarp::sig::Matrix &end_pose,
 					const float &start_time,
 					const float &end_time);
@@ -19,20 +19,20 @@ class CartesianTrajectory
 				const float &time);
 						
 	private:		
-		Quintic quintic_position;							// Trajectory for the position component
-		Quintic quintic_orientation;							// Trajectory for the orientation component
+		Quintic quintic_position;						// Trajectory for the position component
+		Quintic quintic_orientation;						// Trajectory for the orientation component
 		
-		yarp::sig::Matrix R0;								// Starting rotation (needed for later)
+		yarp::sig::Matrix R0;							// Starting rotation (needed for later)
 		
 		// Current state information
-		yarp::sig::Vector pos;								// Position (m)
-		yarp::sig::Vector axisAngle;							// Axis and angle (rad) of rotation
-		yarp::sig::Vector linear_vel;							// Linear velocity (m/s)
-		yarp::sig::Vector linear_acc;							// Linear acceleration(m/s/s)
+		yarp::sig::Vector pos;							// Position (m)
+		yarp::sig::Vector axisAngle;						// Axis and angle (rad) of rotation
+		yarp::sig::Vector linear_vel;						// Linear velocity (m/s)
+		yarp::sig::Vector linear_acc;						// Linear acceleration(m/s/s)
 		yarp::sig::Vector angular_vel;						// Angular velocity (rad/s)
 		yarp::sig::Vector angular_acc;						// Angular velocity (rad/s)
 			
-};												// Semicolon needed after class declaration
+};											// Semicolon needed after class declaration
 
 /******************** Constuctor for 2 poses ********************/
 CartesianTrajectory::CartesianTrajectory(const yarp::sig::Matrix &start_pose,		// Basic constructor
@@ -40,7 +40,7 @@ CartesianTrajectory::CartesianTrajectory(const yarp::sig::Matrix &start_pose,		/
 					const float &start_time,
 					const float &end_time)
 					:
-					R0(start_pose.submatrix(0,2,0,2))			// Initial rotation - needed for later
+					R0(start_pose.submatrix(0,2,0,2))		// Initial rotation - needed for later
 {
 	// Check the inputs are sound
 	if(start_pose.rows() 	!= 4
@@ -63,53 +63,20 @@ CartesianTrajectory::CartesianTrajectory(const yarp::sig::Matrix &start_pose,		/
 	yarp::sig::Vector p1(3), p2(3);
 	for(int i = 0; i < 3; i++)
 	{
-		p1[i] = start_pose[i][3];							// Transfer vectors
+		p1[i] = start_pose[i][3];						// Transfer vectors
 		p2[i] = end_pose[i][3];
 	}
 	this->quintic_position = Quintic(p1, p2, start_time, end_time);			// Create position-based trajectory
 	
 	// For orientation, interpolate over the **difference** dR = R0^T*Rf
 	// such that later we return R(t) = R0*dR(t) => R(tf) = R0*dR*Rf = Rf
-	yarp::sig::Matrix dR(3,3);
-	dR.zero();					
-	for(int i = 0; i < 3; i++)
-	{
-		for(int j = 0; j < 3; j++)
-		{
-			for(int k = 0; k < 3; k++) dR[i][j] += start_pose[k][i]*end_pose[k][j]; // This should be R0^T*Rf
-		}
-	}
-	yarp::sig::Vector axisAngle = yarp::math::dcm2axis(dR);				// Convert SO(3) to axis-angle vector
-	this->quintic_orientation = Quintic(axisAngle, start_time, end_time);		// Create orientation-based trajectory
-
+	this-> R0 = start_pose.submatrix(0,2,0,2);					// Initial orientation
+	yarp::sig::Matrix Rf = end_pose.submatrix(0,2,0,2);				// Final orientation
+	yarp::sig::Matrix dR = R0.transposed()*Rf;					// Difference
 	
-	// dp = pf - p0;
-	// p(t) = p0 + dp(t) -> p(tf) = p0 + dp(tf) = p0 + pf - p0 = pf
+	this->axisAngle = yarp::math::dcm2axis(dR);					// Convert to axis-angle format
 	
-	// dR = R0^-1*Rf
-	// R(t) = R0*dR(t) -> R(tf) = R0*dR(tf) = R0*R0^-1*Rf = Rf
-	
-	
-	/* The code below works just fine:
-	
-	// Instead of interpolating from start_pose -> end_pose, inteprolate across the *difference*
-	// (this makes it easier for the orientation)
-	
-	yarp::sig::Matrix dT = yarp::math::SE3inv(start_pose)*end_pose;			// Tf = T0*dT --> dT = T0.inverse()*Tf
-	//std::cout << dT[0][3] << " " << dT[1][3] << " " << dT[2][3] << std::endl;
-	//std::cout << end_pose[0][3] - start_pose[0][3] << " " << end_pose[1][3] - start_pose[1][3]<< " " << end_pose[2][3]- start_pose[2][3]<< std::endl;
-	
-	yarp::sig::Vector p1(3), p2(3);
-	p1.zero();										// Start from zero
-	p2[0] = dT[0][3];									// This should be end_position - start_position
-	p2[1] = dT[1][3];
-	p2[2] = dT[2][3];
-	yarp::sig::Vector temp = yarp::math::dcm2axis(dT);					// Get the axis-angle encoded in the transform
-	
-	// Create trajectories that interpolate over the difference (starting from zero)
-	this->quintic_position 	= Quintic(p1, p2, start_time, end_time);
-	this->quintic_orientation	= Quintic(temp, start_time, end_time);
-	*/
+	this->quintic_orientation = Quintic(axisAngle, start_time, end_time);		// Create trajectory over axis-angle
 }
 
 /******************** Get the desired state for the given time ********************/
@@ -133,26 +100,22 @@ void CartesianTrajectory::get_state(yarp::sig::Matrix &pose,					// Get the desi
 	this->quintic_position.get_state(this->pos, this->linear_vel, this->linear_acc, time);
 	this->quintic_orientation.get_state(this->axisAngle, this->angular_vel, this->angular_acc, time);
 	
-	// Transfer translation, velocity, acceleration components
-	pose.zero();
-	for(int i = 0; i < 3; i++)
-	{
-		pose[i][3]	= this->pos[i];
-		vel[i] 	= this->linear_vel[i];
-		vel[i+3] 	= this->angular_vel[i];
-		acc[i]		= this->linear_acc[i];
-		acc[i+3]	= this->angular_acc[i];
-	}
-	
 	// Orientation trajectory gives the *difference* dR(t),
 	//need to compute the actual desired state as R(t) = R0*dR(t)
-	yarp::sig::Matrix dR = yarp::math::axis2dcm(this->axisAngle);			// Convert to matrix form
+	yarp::sig::Matrix R = this->R0*yarp::math::axis2dcm(this->axisAngle);
+	
+	// Transfer translation, velocity, acceleration components
 	for(int i = 0; i < 3; i++)
 	{
-		for(int j = 0; j < 3; j++)
-		{
-			for(int k = 0; k < 3; k++) pose[i][j] = this->R0[i][k]*dR[k][j];	// This should be R0*dR
-		}
+		pose[i][3]	= this->pos[i];						// Set translation part of SE3 matrix
+		for(int j = 0; j < 3; j++) pose[i][j] = R[i][j];			// Set rotation part of SE3 matrix
+		vel[i] 		= this->linear_vel[i];					// Set linear velocities
+		vel[i+3] 	= this->angular_vel[i];					// Set angular velocities
+		acc[i]		= this->linear_acc[i];					// Set linear acceleration
+		acc[i+3]	= this->angular_acc[i];					// Set angular acceleration
+		
+		pose[3][i]	= 0.0;							// Ensure that pose is an SE3 matrix
 	}
+		pose[3][3]	= 1.0;
 }
 #endif
