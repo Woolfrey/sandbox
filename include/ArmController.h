a9f65842a0928cee1872aed3cdf6c64e0a08e918
Jon <jonathan.woolfrey@gmail.com>
2021-12-10 17:13:28 +0100

    Added function to get partial derivative of Jacobian

--- include/ArmController.h
+++ include/ArmController.h
@@ -15,6 +15,10 @@ class ArmController :	public MultiJointController
 				
 		yarp::sig::Matrix get_jacobian() {return this->arm.GeoJacobian();}
 		
+		void prep_for_hessian() {this->arm.prepareForHessian();}
+
+		yarp::sig::Matrix get_partial_jacobian(const int &j);			// Return dJ/dq
+		
 		yarp::sig::Matrix get_pose() {return this->arm.getH();}			// Get SE3 matrix for pose of the hand
 		
 		void set_joint_angles(const yarp::sig::Vector &angles) {this->arm.setAng(angles);}
@@ -31,6 +35,12 @@ class ArmController :	public MultiJointController
 		
 		iCub::iKin::iCubArm arm;						// Arm kinematics
 		
+		yarp::sig::Matrix J, dJdq;
+		
+		yarp::sig::Matrix pos;							// Desired pose for the hand
+		
+		yarp::sig::Vector vel, acc;						// Desired velocity, acceleration for the hand
+		
 };											// Semicolon needed after class declaration
 
 /******************** Configure the left arm ********************/
@@ -39,9 +49,9 @@ void ArmController::configure(const std::string &local_port_name,
 				const std::string &_name)
 {	
 	// Resize vectors and matrix
-//	this->pos.resize(4,4);
-//	this->vel.resize(6);
-//	this->acc.resize(6);
+	this->pos.resize(4,4);
+	this->vel.resize(6);
+	this->acc.resize(6);
 	
 	// Set the iCubArm object
 	this->arm = iCub::iKin::iCubArm(_name+"_V3");
@@ -69,10 +79,7 @@ void ArmController::set_cartesian_trajectory(const yarp::sig::Matrix &desired, c
 /******************** Solves the task velocities in the hand frame ********************/
 yarp::sig::Vector ArmController::get_cartesian_control(const double &time)
 {
-	yarp::sig::Matrix pos(4,4);
-	yarp::sig::Vector vel(6), acc(6);
-	
-	this->trajectory.get_state(pos, vel, acc, time);
+	this->trajectory.get_state(this->pos, this->vel, this->acc, time);
 		
 	return vel + 2.0*(get_pose_error(pos, this->arm.getH()));			// Feedforward + feedback
 
@@ -81,14 +88,26 @@ yarp::sig::Vector ArmController::get_cartesian_control(const double &time)
 yarp::sig::Vector ArmController::get_pose_error(const yarp::sig::Matrix &desired,
 						const yarp::sig::Matrix &actual)
 {
-	yarp::sig::Matrix Re = desired.submatrix(0,2,0,2)*actual.submatrix(0,2,0,2).transposed();
-	yarp::sig::Vector axisAngle = yarp::math::dcm2axis(Re);
-	yarp::sig::Vector error(6);
+	yarp::sig::Matrix Re = desired.submatrix(0,2,0,2)*actual.submatrix(0,2,0,2).transposed(); // Rotation error as SO(3)
+	yarp::sig::Vector axisAngle = yarp::math::dcm2axis(Re);				// Convert SO(3) to axis-angle
+	yarp::sig::Vector error(6);							// Vector to be returned
 	for(int i = 0; i < 3; i++)
 	{
-		error[i]	= desired[i][3] - actual[i][3];
-		error[i+3]	= axisAngle[3]*axisAngle[i];
+		error[i]	= desired[i][3] - actual[i][3];				// Translation error
+		error[i+3]	= axisAngle[3]*axisAngle[i];				// Orientation error as angle*axis
 	}
 	
 	return error;
+}
+
+yarp::sig::Matrix ArmController::get_partial_jacobian(const int &j)
+{
+	yarp::sig::Matrix dJdq(6,this->n);
+	
+	for(int i = 0; i < this->n; i++)
+	{
+		dJdq.setCol(i, this->arm.fastHessian_ij(i,j));
+	}
+	
+	return dJdq;
 }
