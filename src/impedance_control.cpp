9a20470438c4727b94792b6feab148ea8e05555a
Jon <jonathan.woolfrey@gmail.com>
2022-01-14 14:02:52 +0100

    Under development

---
+++ src/impedance_control.cpp
@@ -0,0 +1,10 @@
+#include <CartesianTraj.h>
+#include <CubicRot.h>
+#include <Haiku.h>
+#include <QuinticRot.h>
+
+int main(int argc, char *argv[])
+{
+	haiku();
+	return 0;					// No problems with main
+}
