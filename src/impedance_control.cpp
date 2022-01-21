12ea010d3b7a4c9579f984e9c50728790fb17ecf
Jon <jonathan.woolfrey@gmail.com>
2022-01-21 17:40:20 +0100

    Updated header files to test compilation

--- src/impedance_control.cpp
+++ src/impedance_control.cpp
@@ -1,7 +1,9 @@
 #include <CartesianTrajectory.h>
 #include <CubicRotation.h>
 #include <Haiku.h>
+#include <JointController.h>
 #include <QuinticRotation.h>
+#include <Humanoid.h>
 
 int main(int argc, char *argv[])
 {
