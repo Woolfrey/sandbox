c6d8f8e8f08327effb13b8a2072256724e264daa
Jon <jonathan.woolfrey@gmail.com>
2022-02-02 16:06:31 +0100

    Problems on personal laptop - maybe the solution is here?

---
+++ build/CMakeFiles/haiku.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/haiku.dir/src/haiku.cpp.o"
+  "bin/haiku"
+  "bin/haiku.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/haiku.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
