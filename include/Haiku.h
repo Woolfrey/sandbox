872841c7c408d59b6e13db95bb0de8976fc3c240
Jon <jonathan.woolfrey@gmail.com>
2022-02-03 15:20:25 +0100

    Added #ifndef condition

--- include/Haiku.h
+++ include/Haiku.h
@@ -1,8 +1,13 @@
+#ifndef HAIKU_H_
+#define HAIKU_H_
+
 #include <iostream>
 
 void haiku()
 {
-	std::cout << "Worker bees can leave." << std::endl;
+	std::cout << "\nWorker bees can leave." << std::endl;
 	std::cout << "Even drones can fly away." << std::endl;
-	std::cout << "The Queen is their slave." << std::endl;
+	std::cout << "The Queen is their slave.\n" << std::endl;
 }
+
+#endif
